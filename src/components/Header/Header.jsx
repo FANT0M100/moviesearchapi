a33e2c05ed7855e74e4b8f252bf507ac97834440
FANT0M100 <giorgie103188@gmail.com>
2023-02-22 15:00:54 +0400

    make header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return(
+        <div className="logo">
+            <div className="container">
+                <p>Gio's<span>Movie</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
